ec33ba3e92e13c526f69aa0303e76ad7ed78c394
elJuanjoRamos <jjramca1010@gmail.com>
2021-07-02 22:02:09 -0600

    Lecciones iniciales

---
+++ Leccion02-Variables/main.py
@@ -0,0 +1,23 @@
+miVariable = "Hola desde Python";
+print(miVariable)
+# Python no es tipado, no obligatorio
+
+miVariable = 20;
+print(miVariable)
+
+miVariable = True
+print(miVariable)
+
+#########################
+
+x = 10;
+y = 2;
+z = x + y;
+print(z)
+
+#######DIRECCIONES DE MEMORIA##############
+#la funcion id() trae la direccion de memoria de una variable o elemento
+print( id(x) )
+print( id(y) )
+print( id(z) )
+
---
+++ Leccion03-TiposDatos/main.py
@@ -0,0 +1,26 @@
+#TIPOS BASICOS
+
+x = 10
+print( x )
+#la funcion type() trae el tipo de datos de una variable
+print( type(x) )
+
+y: str = "hola mundo"
+print( type(y) )
+
+z: float = 4.5
+print( type(z) )
+
+w: bool = True
+print( type(w) )
+
+
+#MANEJOS DE CADENAS
+
+miGrupoFav: str = "Metalica"
+comentario:str = "Mi grupo favorito"
+print(comentario, miGrupoFav)
+
+
+
+
