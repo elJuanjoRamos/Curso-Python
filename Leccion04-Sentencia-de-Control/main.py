0a11d2b6b0a4c03b7ad4fdd492ce555584714ed0
Juan Jose Ramos <eljuanjo.ramos@gmail.com>
2021-07-15 10:29:29 -0600

    Update main.py

--- Leccion04-Sentencia-de-Control/main.py
+++ Leccion04-Sentencia-de-Control/main.py
@@ -12,7 +12,26 @@ elif 20 <= edad < 30:
     mensaje = 'Amor y comienza el trabajo...'
 
 
-print(f'Tu edad es { edad }, ', mensaje )
+print(f'Tu edad es { edad }, ', mensaje)
 
 
+
+
+calificacion = int(input('Proporciona un valor entre 0 y 10: ' ))
+letra: str = None
+
+if 9<= calificacion < 10 :
+    letra = 'A'
+elif 8<= calificacion < 9 :
+    letra = 'B'
+elif 7 <= calificacion < 8 :
+    letra = 'C'
+elif 6<= calificacion < 7 :
+    letra = 'D'
+elif 0<= calificacion < 6 :
+    letra = 'F'
+else:
+    letra = 'Desconocida'
+
+print(f'Tu calificacion fue de { calificacion }, tiene una letra { letra } ')
 
